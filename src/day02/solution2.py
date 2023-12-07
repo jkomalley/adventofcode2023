61b56daefd68ba1c49129d4c58f08c3ccdd92615
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2023-12-07 01:00:53 +0000

    finish day 2 part b

--- src/day02/solution2.py
+++ src/day02/solution2.py
@@ -1,19 +1,14 @@
+from math import prod
 import re
 from typing import TextIO
 import click
 
 
-def getGameNum(line):
-    return re.search(r"\d+", line).group()
-
-
 def get(s, color):
     ret = 0
-    # print(f"{s = } : {color = }")
     regex = rf"\d+ {color}"
-    val = re.search(regex, s)
 
-    # print(val)
+    val = re.search(regex, s)
 
     if val:
         val = re.match(r"\d+", val[0])
@@ -21,52 +16,40 @@ def get(s, color):
     if val:
         ret = int(val[0])
 
-    # print(f"Found {ret} {color}")
-
     return ret
 
 
-def isPossible(line, r, g, b):
+def getMinColors(line: str) -> tuple[int, int, int]:
     sets = line.split("; ")
+    minR = 0
+    minG = 0
+    minB = 0
     for s in sets:
         rNum = get(s, "red")
         gNum = get(s, "green")
         bNum = get(s, "blue")
-        if rNum > r or gNum > g or bNum > b:
-            # print(f"Not possible: {rNum = } {gNum = } {bNum = }")
-            return False
-        # print(f"Possible: {rNum = } {gNum = } {bNum = }")
-    return True
+        
+        minR = max(minR, rNum)
+        minG = max(minG, gNum)
+        minB = max(minB, bNum)
+
+    return (minR, minG, minB)
 
 
 @click.command()
 @click.option("-i", "--input", required=True, type=click.File("r"))
-@click.option("-r", default=12)
-@click.option("-g", default=13)
-@click.option("-b", default=14)
-def main(input: TextIO, r, g, b):
+def main(input: TextIO):
     print("Advent of Code Day 2 Part A")
 
     data = input.readlines()
 
-    # data = ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-    #         "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-    #         "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-    #         "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-    #         "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-    #         ]
-
     total = 0
     for line in data:
-        gameNum = getGameNum(line)
-        ret = isPossible(line[8:], r, g, b)
+        ret = getMinColors(line[8:])
+        power = prod(ret)
+        total += power
 
-        print(f"Game {gameNum}: {ret}")
-
-        if ret:
-            total += int(gameNum)
-
-    print(f"{total = }")
+    print(f"answer = {total}")
 
 
 if __name__ == "__main__":
